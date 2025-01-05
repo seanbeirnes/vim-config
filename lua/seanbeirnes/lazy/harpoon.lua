272e2fd6edde4376c33fcb959237f261fd6e43d4
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2025-01-04 23:31:13 -0500

    Added harpoon package

---
+++ lua/seanbeirnes/lazy/harpoon.lua
@@ -0,0 +1,23 @@
+return {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+            local harpoon = require("harpoon")
+
+            harpoon:setup()
+
+            vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end)
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+            vim.keymap.set("n", "<C-f>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-d>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<C-a>", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<leader><C-f>", function() harpoon:list():replace_at(1) end)
+            vim.keymap.set("n", "<leader><C-d>", function() harpoon:list():replace_at(2) end)
+            vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(3) end)
+            vim.keymap.set("n", "<leader><C-a>", function() harpoon:list():replace_at(4) end)
+        end
+}
