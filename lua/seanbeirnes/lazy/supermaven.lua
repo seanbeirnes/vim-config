ebe0ee0c23dd32d32917479cf9634e87869c497b
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2025-07-04 13:10:25 -0400

    Update Supermaven toggle

--- lua/seanbeirnes/lazy/supermaven.lua
+++ lua/seanbeirnes/lazy/supermaven.lua
@@ -1,13 +1,28 @@
+local function toggle_supermaven()
+    if package.loaded["supermaven-nvim"] then
+        local api = require("supermaven-nvim.api")
+        api.toggle()
+        if api.is_running() then
+            print("Supermaven: ON")
+        else
+            print("Supermaven: OFF")
+        end
+    else
+        print("Supermaven: Cannot toggle, plugin not loaded")
+    end
+end
+
 return {
     {
         "supermaven-inc/supermaven-nvim",
+        lazy = false,
         config = function()
-        require("supermaven-nvim").setup({})
+            require("supermaven-nvim").setup({})
         end,
         keys = {
             {
                 "<leader>sm",
-                "<cmd>SupermavenToggle<cr>",
+                toggle_supermaven,
                 desc = "toggle supermaven-nvim",
             },
         },
