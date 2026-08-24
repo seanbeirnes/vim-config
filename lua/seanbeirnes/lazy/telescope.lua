42845b9cb652b247d6750b3f3f8bfb32a5f688a3
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2026-08-24 01:22:28 -0400

    feat: add shortcut for telescope git_status

--- lua/seanbeirnes/lazy/telescope.lua
+++ lua/seanbeirnes/lazy/telescope.lua
@@ -13,6 +13,7 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
@@ -27,4 +28,3 @@ return {
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
-
